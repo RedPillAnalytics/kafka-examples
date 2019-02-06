f4745e8dc54d46b4fa91840105f66f8e2dd3e48d
Stewart Bryson <stewart@redpillanalytics.com>
2019-02-05 22:29:13 -0500

    initial commit

---
+++ src/main/pipeline/01-clickstream/01-create.sql
@@ -0,0 +1,9 @@
+--SOURCE of ClickStream
+CREATE STREAM clickstream (_time bigint,time varchar, ip varchar, request varchar, status int, userid int, bytes bigint, agent varchar) 
+with (kafka_topic = 'clickstream', value_format = 'json');
+
+--BUILD STATUS_CODES
+-- it's fne to have multiple comment lines
+-- everybody's doing it
+CREATE TABLE clickstream_codes (code int, definition varchar) 
+with (key='code', kafka_topic = 'clickstream_codes', value_format = 'json');
