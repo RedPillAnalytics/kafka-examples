35e0863cff381309c58049d8d3bcebfde79edf63
Stewart Bryson <stewart@redpillanalytics.com>
2019-03-27 00:55:52 -0400

    Code samples updated.

--- functions/src/test/groovy/DecodeTest.groovy
+++ functions/src/test/groovy/DecodeTest.groovy
@@ -12,55 +12,48 @@ class DecodeTest extends Specification {
    @Unroll
    def "When: #expression, #search, #text, #defaultValue; Expect: #result"() {
 
-      expect: "#result is returned with a successful match"
+      expect:
       decode.decode(expression, search, text, defaultValue) == result
 
       where:
-
       expression    | search        | text  | defaultValue || result
       'KSQL Rocks!' | 'ksql rocks!' | 'yes' | 'no'         || 'no'
       'KSQL Rocks!' | 'KSQL Rocks!' | 'yes' | 'no'         || 'yes'
-
    }
 
    @Unroll
    def "When: #expression, #search, #text, #defaultValue, #ignorecase; Expect: #result"() {
 
-      expect: "#result is returned with a successful match"
+      expect:
       decode.decode(expression, search, text, defaultValue, ignorecase) == result
 
       where:
-
       expression    | search        | text  | defaultValue | ignorecase || result
       'KSQL Rocks!' | 'KSQL rocks!' | 'yes' | 'no'         | true       || 'yes'
       'KSQL Rocks!' | 'KSQL Rocks!' | 'yes' | 'no'         | true       || 'yes'
       'KSQL Rocks!' | 'KSQL rocks!' | 'yes' | 'no'         | false      || 'no'
-
    }
 
    @Unroll
    def "When: #expression, #search1, #text1, #search2, #text2, #defaultValue, #ignorecase; Expect: #result"() {
 
-      expect: "#result is returned with a successful match"
+      expect:
       decode.decode(expression, search1, text1, search2, text2, defaultValue, ignorecase) == result
 
       where:
-
       expression    | search1       | text1 | search2       | text2 | defaultValue | ignorecase || result
       'KSQL Rocks!' | 'KSQL rocks!' | 'yes' | 'KSQL Sucks!' | 'no'  | 'no'         | true       || 'yes'
       'KSQL Rocks!' | 'KSQL rocks!' | 'no'  | 'KSQL Rocks!' | 'yes' | 'no'         | false      || 'yes'
       'KSQL Rocks!' | 'KSQL rocks!' | 'no'  | 'KSQL Sucks!' | 'no'  | 'yes'        | false      || 'yes'
-
    }
 
    @Unroll
    def "When: #expression, #search1, #text1, #search2, #text2, #search3, #text3, #defaultValue, #ignorecase; Expect: #result"() {
 
-      expect: "#result is returned with a successful match"
+      expect:
       decode.decode(expression, search1, text1, search2, text2, search3, text3, defaultValue, ignorecase) == result
 
       where:
-
       expression    | search1       | text1 | search2       | text2 | search3       | text3 | defaultValue | ignorecase || result
       'KSQL Rocks!' | 'KSQL rocks!' | 'yes' | 'KSQL Sucks!' | 'no'  | 'KSQL, meh'   | 'no'  | 'no'         | true       || 'yes'
       'KSQL Rocks!' | 'KSQL rocks!' | 'no'  | 'KSQL Rocks!' | 'yes' | 'KSQL, meh'   | 'no'  | 'no'         | false      || 'yes'
